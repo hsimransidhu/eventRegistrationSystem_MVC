c4a27ef2ea7d3ccde53e615bdd9ca4727c85437d
Harsimran jot Kaur <119031600+hsimransidhu@users.noreply.github.com>
2024-03-01 01:22:44 -0600

    Update EventController.cs

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -1,4 +1,4 @@
-﻿using ERM_BLL;
+ using ERM_BLL;
 using ERM_ENTITIES;
 using System;
 using System.Collections.Generic;
@@ -8,46 +8,50 @@ using System.Web.Mvc;
 
 namespace ERS.Controllers
 {
+    /// <summary>
+    /// This controller manages CRUD operations for events.
+    /// </summary>
     public class EventController : Controller
     {
-    
+        /// <summary>
+        /// Displays a list of all events.
+        /// </summary>
         public ActionResult Index()
         {
-           
             EventService eventService = new EventService();
-
             var events = eventService.GetAllEvents();
-
-           
             return View(events);
         }
 
-   
+        /// <summary>
+        /// Displays details of a specific event.
+        /// </summary>
         public ActionResult Details(int id)
         {
             return View();
         }
 
- 
+        /// <summary>
+        /// Renders a form to create a new event.
+        /// </summary>
         public ActionResult Create()
         {
             return View();
         }
- 
+
+        /// <summary>
+        /// Handles form submission for creating a new event.
+        /// </summary>
         [HttpPost]
         public ActionResult Create(Event e)
         {
             try
             {
-               
                 EventService eventService = new EventService();
-
-          
                 if (eventService.AddEventService(e))
                 {
                     ViewBag.Message = "Event added successfully";
                     return View();
-                    
                 }
                 else
                 {
@@ -59,30 +63,30 @@ namespace ERS.Controllers
                 return View();
             }
         }
- 
-        public ActionResult Edit(int id)
-        { 
-            EventService eventService = new EventService();
 
+        /// <summary>
+        /// Renders a form to edit an existing event.
+        /// </summary>
+        public ActionResult Edit(int id)
+        {
+            EventService eventService = new EventService();
             var Eventt = eventService.GetAllEvents().Find(e => e.EventId == id);
- 
             return View(Eventt);
         }
- 
+
+        /// <summary>
+        /// Handles form submission for editing an event.
+        /// </summary>
         [HttpPost]
         public ActionResult Edit(Event e)
         {
             try
             {
-                
                 EventService eventService = new EventService();
-
-                
                 if (eventService.UpdateEventService(e))
                 {
                     ViewBag.Message = "Event updated successfully";
                     return View();
-                 
                 }
                 else
                 {
@@ -95,12 +99,12 @@ namespace ERS.Controllers
             }
         }
 
-       
+        /// <summary>
+        /// Deletes the specified event.
+        /// </summary>
         public ActionResult Delete(int id)
         {
-        
             EventService eventService = new EventService();
- 
             if (eventService.DeleteEventService(id))
             {
                 ViewBag.Message = "Event deleted successfully";
@@ -112,14 +116,15 @@ namespace ERS.Controllers
                 return RedirectToAction("Index");
             }
         }
- 
+
+        /// <summary>
+        /// Handles form submission for deleting an event.
+        /// </summary>
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
             try
             {
-               
-
                 return RedirectToAction("Index");
             }
             catch
