c27f2dd45f457136fc1533656533710d60993f76
hsimransidhu <simusidhu2004@gmail.com>
2024-02-29 23:54:11 -0600

    Add project files.

---
+++ Controllers/EventController.cs
@@ -0,0 +1,132 @@
+﻿using ERM_BLL;
+using ERM_ENTITIES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERS.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+           
+            EventService eventService = new EventService();
+
+            var events = eventService.GetAllEvents();
+
+           
+            return View(events);
+        }
+
+        // GET: Event/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Event/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Event/Create
+        [HttpPost]
+        public ActionResult Create(Event e)
+        {
+            try
+            {
+               
+                EventService eventService = new EventService();
+
+          
+                if (eventService.AddEventService(e))
+                {
+                    ViewBag.Message = "Event added successfully";
+                    return View();
+                    
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+ 
+        public ActionResult Edit(int id)
+        { 
+            EventService eventService = new EventService();
+
+            var Eventt = eventService.GetAllEvents().Find(e => e.EventId == id);
+ 
+            return View(Eventt);
+        }
+ 
+        [HttpPost]
+        public ActionResult Edit(Event e)
+        {
+            try
+            {
+                
+                EventService eventService = new EventService();
+
+                
+                if (eventService.UpdateEventService(e))
+                {
+                    ViewBag.Message = "Event updated successfully";
+                    return View();
+                 
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+       
+        public ActionResult Delete(int id)
+        {
+        
+            EventService eventService = new EventService();
+ 
+            if (eventService.DeleteEventService(id))
+            {
+                ViewBag.Message = "Event deleted successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Message = "Event not deleted";
+                return RedirectToAction("Index");
+            }
+        }
+ 
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+               
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,108 @@
+﻿using ERM_BLL;
+using ERM_ENTITIES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERS.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public ActionResult Index(int eventId, string eventName)
+        {
+            if (eventName == null)
+            {
+                EventService es = new EventService();
+                var events = es.GetAllEvents();
+                eventName = events.FirstOrDefault(e => e.EventId == eventId).EventName;
+            }
+            RegistrationService registrationService = new RegistrationService();
+            var registrations = registrationService.GetAllRegistrations(eventId);
+
+            TempData["eventId"] = eventId;
+            TempData["eventName"] = eventName;
+
+            return View(registrations);
+        }
+
+
+        public ActionResult Create()
+        {
+            Registration registration = null;
+
+            int eventId = (int)TempData["eventId"];
+
+            if (eventId != 0)
+                registration = new Registration() { EventId = eventId, RegistrationDate = null };
+
+            EventService es = new EventService();
+            var events = es.GetAllEvents();
+
+            var eventItems = events.Select(e => new SelectListItem
+            {
+                Text = e.EventName,
+                Value = e.EventId.ToString()
+            }).ToList();
+            TempData["eventsList"] = eventItems;
+
+            TempData.Keep();
+
+            return View(registration);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Registration registration)
+        {
+            RegistrationService registrationService = new RegistrationService();
+            
+            if (registrationService.AddRegistrationService(registration))
+            {
+                ViewBag.Message = "Registration is added successfully";
+            }
+
+            return View(registration);
+        }
+
+
+        public ActionResult Edit(int registrationId)
+        {
+            int eventId = (int)TempData["eventId"];
+
+            RegistrationService registrationService = new RegistrationService();
+            var registration = registrationService.GetAllRegistrations(eventId).Find(x => x.RegistrationId == registrationId);
+
+    
+            return View(registration);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Registration registration)
+        {
+            RegistrationService registrationService = new RegistrationService();
+            if (registrationService.UpdateRegistrationService(registration))
+            {
+                return RedirectToAction("Index", new { eventId = registration.EventId });
+            }
+            else
+            {
+                return View(registration);
+            }
+        }
+
+
+        public ActionResult DeleteRegistration(int registrationId )
+        {
+          
+            RegistrationService registrationService = new RegistrationService();
+
+            if (registrationService.DeleteRegistrationService(registrationId))
+            {
+                ViewBag.Message = "Success";
+            }
+            return RedirectToAction("Index", new { eventId = TempData["EventId"] });
+        }
+    }
+
+}
